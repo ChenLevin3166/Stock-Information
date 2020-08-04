667afdfbc67857b2935751dbf69116f44b98ae54
ChenLevin3166 <henc1995@gmail.com>
2020-08-04 11:38:45 +0300

    Updates

---
+++ precache-manifest.02b96e4c76066edf087e3b59276023e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7b2cbe79fe7837e504a4376f867006d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86e17c7de1865877d0dc",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ec2a8571a8a53eb46fba",
+    "url": "/static/css/main.e8488883.chunk.css"
+  },
+  {
+    "revision": "86e17c7de1865877d0dc",
+    "url": "/static/js/2.6b313026.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/static/js/2.6b313026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec2a8571a8a53eb46fba",
+    "url": "/static/js/main.68af1b1f.chunk.js"
+  },
+  {
+    "revision": "64c3fb8672980db25013",
+    "url": "/static/js/runtime-main.f5a68994.js"
+  }
+]);
